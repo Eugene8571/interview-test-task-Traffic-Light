d7378fe2103fd7c852e1055ffb52cd2a07ae707f
Eugene8571 <40310109+Eugene8571@users.noreply.github.com>
2021-03-10 03:16:34 +0200

    Update admin.py

--- employee_tree/list_of_employees/admin.py
+++ employee_tree/list_of_employees/admin.py
@@ -2,4 +2,16 @@ from django.contrib import admin
 
 # Register your models here.
 from list_of_employees.models import Employees
-admin.site.register(Employees)
+
+class EmployeessProfileAdmin(admin.ModelAdmin):
+    list_display = ['employee', 'position', 'department', 'salary', 'date_of_employment']
+
+    def employee(self, obj):
+        return obj.last_name + ' '\
+         + obj.first_name[0] + '. '\
+          + obj.middle_name[0] + '.'
+
+    employee.short_description = 'ФИО'
+
+admin.site.register(Employees, EmployeessProfileAdmin)
+
